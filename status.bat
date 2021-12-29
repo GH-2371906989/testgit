b3ebf24844c74a526d05cf7c9b634b41549ad252
guhuan <2371906989@qq.com>
2021-12-29 11:03:26 +0800

    添加脚本

--- status.bat
+++ status.bat
@@ -1,23 +1,45 @@
 @echo off&Setlocal enabledelayedexpansion
 if exist .git (
-echo 查看状态
-	git status
-echo 查看更改的值
-	git diff
-	set /p input=e 退出，(回车  执行 git add . 命令)
+	:loop
+	set input=""
+	set /p input=e 退出，s 查看状态，d 查看内容变化，add 添加到缓存，comm 添加到仓库，push 添加到远程
+
+cls 
+
 	if !input! == e (
 	goto ex
 	)
-	git add .
-	echo 查看状态
+
+	if !input! == s (
+	echo ">>git status" 
 	git status
-	set /p cinput=请输入存储描述：(回车结束)
-	if "!cinput!" == "" (
-	goto ex
 	)
-	git commit -m "!cinput!"
-	echo 查看状态
-	git status
+	
+	if !input! == d (
+	echo ">>git diff" 
+	git diff
+	)
+
+	if !input! == add (
+	echo ">>git add ." 
+	git add .
+	)
+
+	if !input! == comm (
+	set /p describe=请输入描述
+	if "!describe!" == "" (
+	goto loop
+	)
+	git commit -m "!describe!"
+	)
+	
+	if !input! == push (
+	echo ">>git push origin master" 
+	git push origin master
+	)
+	goto loop
+
+	
 )
 :ex
 pause
